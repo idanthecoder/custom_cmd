b058d2ada1afa5e0cc9ef7a096a0e8f16ea373dc
Omer <godmega67@gmail.com>
2023-09-22 18:32:31 +0300

    Added try and except to ls due to a bug

--- main.py
+++ main.py
@@ -80,14 +80,18 @@ def main():
     while run:
         prompt = pwd()
         if prompt.lower().__contains__("ls"):
-            if prompt.lower() == "ls":
-                print(ls(r"*"))
-            else:
-                # needs fixing
-                split_data = prompt.lower().split(" ")
-                os.chdir(split_data[1])
-                print(ls(r"*"))
-                os.chdir(original_dir)
+            try:
+                if prompt.lower() == "ls":
+                    print(ls(r"*"))
+                else:
+                    # needs fixing
+                    split_data = prompt.lower().split(" ")
+                    os.chdir(split_data[1])
+                    print(ls(r"*"))
+                    os.chdir(original_dir)
+            except OSError:
+                print("Syntax Incorrect!")
+                continue
         elif prompt.lower() == "exit":
             exit_cmd()
         elif prompt.lower().__contains__("cd"):
