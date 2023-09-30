405396c62bf174c9db7184b6bbd58ae1a44a0f92
Idan The Coder <barkinidan@gmail.com>
2023-09-30 15:57:36 +0300

    remove comments

--- echo.py
+++ echo.py
@@ -1,7 +1,3 @@
-# This is a sample Python script.
-
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 
 
 def main():
@@ -16,8 +12,5 @@ def main():
             print("end of script")
 
 
-# Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     main()
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
