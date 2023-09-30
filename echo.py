c2f08cacb2a92284711c7d0014064f85c7b375cc
Idan The Coder <barkinidan@gmail.com>
2023-09-30 15:53:58 +0300

    added echo

---
+++ echo.py
@@ -0,0 +1,23 @@
+# This is a sample Python script.
+
+# Press Shift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+
+
+def main():
+    run = True
+
+    while run:
+        word = input("pls enter input:\n")
+        print(f"you wrote: {word}")
+
+        if word == "exit":
+            run = False
+            print("end of script")
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    main()
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
